3236d8599b5f7c7859bbc0200691fd795de57da3
c0b22039 <c0b22039da@edu.teu.ac.jp>
2023-11-28 14:34:12 +0900

    爆弾を動かす

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,21 +3,27 @@ import pygame as pg
 import random
 
 WIDTH, HEIGHT = 1600,900
-
-
+delta = {  # 練習３：押下キーと移動量の辞書
+    pg.K_UP: (0, -5),  # キー：移動量／値：（横方向移動量，縦方向移動量）
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900,400
     bb_img = pg.Surface((20,20))
     bb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)        
-    bb_rct=bb_img.get_rect()
-    bb_rct.centerx=random.randint(0,WIDTH)
-    bb_rct.centery=random.randint(0,HEIGHT)
-    vx,vy = +5,+5
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0,WIDTH)
+    bb_rct.centery = random.randint(0,HEIGHT)
+    vx,vy = +5, +5
     
     clock = pg.time.Clock()
     tmr = 0
@@ -25,8 +31,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        # key_lst=pg.key.get_pressed()
+        # sum_mv=[0,0]
+        # for k, tpl in delta.items():
+            
+            
+            
 
         screen.blit(bg_img, [0, 0])
+        kk_rct
         screen.blit(kk_img, [900, 400])
         bb_rct.move_ip(vx,vy)
         screen.blit(bb_img, bb_rct)

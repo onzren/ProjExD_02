0cba77f4359e77705920446e6a183404fe610f16
c0b22039 <c0b22039da@edu.teu.ac.jp>
2023-11-28 14:40:01 +0900

    こうかとんを動かす

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -32,16 +32,19 @@ def main():
             if event.type == pg.QUIT: 
                 return
             
-        # key_lst=pg.key.get_pressed()
-        # sum_mv=[0,0]
-        # for k, tpl in delta.items():
-            
+        key_lst=pg.key.get_pressed()
+        sum_mv=[0,0]
+        for k, tpl in delta.items():
+            if key_lst[k]:  # キーが押されたら
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
             
             
 
         screen.blit(bg_img, [0, 0])
-        kk_rct
         screen.blit(kk_img, [900, 400])
+        kk_rct.move_ip(sum_mv[0],sum_mv[1])
+        screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx,vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
